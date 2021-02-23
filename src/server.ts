ab8a4a0c095297ab430e4ba51d72405761e02696
João Bispo <vitor.vi10@hotmail.com>
2021-02-23 11:15:50 -0300

    feat: added create user resource

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,16 @@
 import express from 'express';
 
 import "./database";
+import { router } from './routes';
 
 const app = express();
 
 const PORT = 3333;
 
-app.get('/users', (req, res) => {
-  res.send('ok')
-});
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true}));
+
+app.use(router);
 
 app.listen(PORT, () => console.log(`server is running at port ${PORT}`));
