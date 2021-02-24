10fbc2bf2a9dd933bae71af4068bee01f9f45070
João Bispo <vitor.vi10@hotmail.com>
2021-02-24 09:57:57 -0300

    feat: added survey creation

--- src/models/Survey.ts
+++ src/models/Survey.ts
@@ -1,7 +1,7 @@
 import { Column, CreateDateColumn, Entity, PrimaryColumn } from "typeorm";
 import { v4 as uuid } from 'uuid';
 
-@Entity("users")
+@Entity("surveys")
 class Survey {
 
   @PrimaryColumn()
--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,14 @@
 import { Router } from 'express';
-import {UserController} from "./controllers/UserController";
+import { SurveysController } from './controllers/SurveysController';
+import { UserController } from "./controllers/UserController";
 
 
 const router = Router();
 
 const userController = new UserController();
+const surveyController = new SurveysController();
 
 router.post("/users", userController.create);
+router.post("/surveys", surveyController.create);
 
 export { router };
